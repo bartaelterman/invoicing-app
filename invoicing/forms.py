7dae281a38cafb74fddaee2a872724a49b476e51
bartaelterman <bart.aelterman@gmail.com>
2017-07-20 09:42:58 +0200

    add jQuery datepicker to timesheet form

--- invoicing/forms.py
+++ invoicing/forms.py
@@ -4,8 +4,8 @@ from django import forms
 
 class TimesheetForm(forms.Form):
     project = forms.ModelChoiceField(label='Project', queryset=Project.objects.all())
-    start = forms.DateField(label='Start date')
-    end = forms.DateField(label='End date')
+    start = forms.DateField(label='Start date', widget=forms.TextInput(attrs={'class':'datepicker'}))
+    end = forms.DateField(label='End date', widget=forms.TextInput(attrs={'class':'datepicker'}))
     unit = forms.ChoiceField(choices=(('days', 'days'), ('hours', 'hours')))
 
 
